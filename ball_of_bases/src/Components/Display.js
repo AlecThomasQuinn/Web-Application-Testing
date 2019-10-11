b6d917ce536ce6160c3c27a87957a179c7210f17
unknown <alec.thomas.quinn@gmail.com>
2019-10-10 20:16:27 -0500

    committing for stand-up

--- ball_of_bases/src/Components/Display.js
+++ ball_of_bases/src/Components/Display.js
@@ -5,7 +5,7 @@ export default function Display(props) {
     return(
         <>
             <h1>Strikes: {props.strikeCount}</h1>
-            <h1>Strikes: {props.ballCount}</h1>
+            <h1>Balls: {props.ballCount}</h1>
         </>
     );
 };
